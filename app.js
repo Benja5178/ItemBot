3efc2c715c153d8e29081a4370ae4705f80f8c2e
Benja5178 <32654932+Benja5178@users.noreply.github.com>
2017-10-09 18:34:58 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const  client = new Discord.Client();
+const config = require("./config.json");
+
+client.on("ready", () => {
+   console.log("Estoy listo!");
+
+   client.user.setGame(prefix+'hi im ItemBot |Creator BenjaWanders ');
+   
+
+});
+var prefix = config.prefix;
+
+client.on("message", (message) => {
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(prefix + "hola")) {
+    message.channel.send("Hola que tal?");
+  }
+});
+client.login(config.token);    
